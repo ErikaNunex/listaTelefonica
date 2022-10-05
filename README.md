51b8f6122b310d61e6c9c45960612025a51d2fef
ErikaNunex <erikanunes0512@gmail.com>
2022-10-04 21:25:23 -0300

    concluído

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # lista_Telef-nica-
 Exercício de conclusão de modulo Java script.
+
+Orientações para execução: 
+
+- O json-server dessa aplicação deve ser rodado na porta 3000 usando o seguinte comando:
+"npx json-serve db.json --port 3000"
+
+- Caso queira usar o insonia para testar a simulação da API use:
+ GET: http://localhost:3000/contato
+ POST: http://localhost:3000/contato
+ PATCH: http://localhost:3000/contato/id
+ DELETE: http://localhost:3000/contato/id 
+
+
+Observações: 
+Essa aplicação foi desenvolvida com a intenção de um crud funcional, não levando em consideração a aparência da página.
+
--- eventos.js
+++ eventos.js
@@ -4,7 +4,7 @@ let API_URL = 'http://localhost:3000'
 function adicionarNovoContato(){
     event.preventDefault();
     
-    let informcoes ={
+    let informacoes ={
         nome: nome.value,
         sobrenome: sobrenome.value,
         cell: cell.value, 
@@ -13,7 +13,7 @@ function adicionarNovoContato(){
 
     fetch(API_URL + '/contato', {
        method: 'POST',
-       body:JSON.stringify(informcoes),
+       body:JSON.stringify(informacoes),
        headers: {
         'Content-Type': 'application/json'
        }
@@ -23,6 +23,9 @@ function adicionarNovoContato(){
     formAdd.resert();
 }
 
+/** Busca os dados na API para serem editados na função editarContato
+ * @param id: o id de cada contato é usado pra identificar o contato que está sendo buscado
+ */
 function buscarParaEditar(id){
     fetch(API_URL+'/contato/'+id)
         .then(resposta => resposta.json()) 
@@ -36,6 +39,7 @@ function buscarParaEditar(id){
 
 }
 
+/**Edita os dados coletados na gunção buscarParaEditar */
 function editarContato(){
     event.preventDefault();
     
@@ -45,7 +49,7 @@ function editarContato(){
         cell:inputEditarCell.value,
         cidade:inputEditarCidade.value,
     };
-    fetch(API_URL+'/contatos/'+inputEditarId.value,{
+    fetch(API_URL+'/contato/'+inputEditarId.value,{
         method: 'PATCH',
         body: JSON.stringify(dados),
         headers:{
@@ -54,11 +58,14 @@ function editarContato(){
     })
         .then(resposta => resposta.json())
         .then(() => atualizarContato());
-    let x =document.querySelector('[data-bs-dismiss="offcanvas"]');
-    x.dispatchEvent(new Event('click'));
+        let x = document.querySelector('[data-bs-dismiss="offcanvas"]');
+        x.dispatchEvent(new Event('click'));
+        
 }
 
-/**exclui todas as informações dos contatos */
+/**exclui todas as informações dos contatos 
+ * @param id: Identifica de qual contato deve ser exclídas as informações
+*/
 async function excluirContato(id){
     let resposta = confirm('voce tem certeza?')
     if(resposta !== true){
@@ -86,9 +93,9 @@ function atualizarContato (){
                 <td>${cadaItem.cidade}</td>
 
                 <td><button onclick="buscarParaEditar(${cadaItem.id})"data-bs-toggle="offcanvas" 
-                data-bs-target="#offcanvasEditar"class="btn btn-warning">Editar</button></td>
+                data-bs-target="#offcanvasEditar"class="btn btn-warning btn-md">Editar</button>
 
-                <td><button class="btn btn-danger" onclick=excluirContato(${cadaItem.id})>Excluir</button></td>
+                <button class="btn btn-danger" onclick=excluirContato(${cadaItem.id})>Excluir</button></td>
             </tr>`
             })
         }
